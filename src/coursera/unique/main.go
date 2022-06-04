37c3a5c00c04984cc9517082843f17e5f2be030e
Makarov Andrey <winandfx@gmail.com>
2022-06-05 01:04:30 +0300

    Пример: Программа уникализации (тесты)

--- src/coursera/unique/main.go
+++ src/coursera/unique/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bufio" //буферизованный ввод/вывод
 	"fmt"
+	"io"
 	"os" //стандартный ввод
 )
 
-func main() {
-	// echo "hello" | go run main.go
-	// cat text.txt | go run main.go
-	in := bufio.NewScanner(os.Stdin)
+//Логика вынесена в отдельную функцию для упрощения написания тестов
+func uniq(input io.Reader, output io.Writer) error {
+	in := bufio.NewScanner(input)
 	var prev string
 	for in.Scan() { //построчный ввод
 		txt := in.Text()
@@ -19,11 +19,21 @@ func main() {
 		}
 
 		if txt < prev {
-			panic("File not sorted: txt==" + txt + ", prev==" + prev)
+			return fmt.Errorf("file not sorted: txt==%v, prev==%v", txt, prev)
 		}
 
 		prev = txt
 
-		fmt.Println(">>", txt)
+		fmt.Fprintln(output, txt)
+	}
+	return nil
+}
+
+func main() {
+	// echo "hello" | go run main.go
+	// cat text.txt | go run main.go
+	err := uniq(os.Stdin, os.Stdout)
+	if err != nil {
+		panic(err.Error())
 	}
 }
