2d805b6ba6f32d6d8b17edfda93ee638e4864ae6
Makarov Andrey <winandfx@gmail.com>
2022-06-17 23:56:54 +0300

    Домашнее задание 2 (шаблон)

---
+++ src/coursera/hw2_signer/signer.go
@@ -0,0 +1,17 @@
+package main
+
+func ExecutePipeline(jobs ...job) {
+
+}
+
+func SingleHash(in chan interface{}, out chan interface{}) {
+
+}
+
+func MultiHash(in chan interface{}, out chan interface{}) {
+
+}
+
+func CombineResults(in chan interface{}, out chan interface{}) {
+
+}
