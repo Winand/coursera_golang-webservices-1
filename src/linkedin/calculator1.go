585218206e7ceedc1a47efad7f35128ae3421151
Andrey Makarov <winandfx@gmail.com>
2022-07-02 00:34:46 +0300

    Calculator: read input with bufio

--- src/linkedin/calculator1.go
+++ src/linkedin/calculator1.go
@@ -3,17 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	val1 := readFloat("Input 1st value: ")
-	val2 := readFloat("Input 2st value: ")
+	val1 := readFloat("Input 1st value")
+	val2 := readFloat("Input 2st value")
 	fmt.Printf("Sum is %v\n", val1+val2)
 }
 
 func readFloat(prompt string) float64 {
 	var fval float64
-	fmt.Print(prompt)
+	fmt.Print(prompt + ": ")
 	_, err := fmt.Scanln(&fval)
 	if err != nil {
 		panic("Wrong input!")
@@ -21,7 +24,13 @@ func readFloat(prompt string) float64 {
 	return fval
 }
 
-func readFloat2(prompt) float64 {
-	reader := bufio.NewReader()
-	inp, _ := reader.ReadLine()
+func readFloatBufio(prompt string) float64 {
+	fmt.Print(prompt + ": ")
+	reader := bufio.NewReader(os.Stdin)
+	inp, _ := reader.ReadString('\n')
+	fval, err := strconv.ParseFloat(strings.TrimSpace(inp), 64)
+	if err != nil {
+		panic("Wrong input!")
+	}
+	return fval
 }
