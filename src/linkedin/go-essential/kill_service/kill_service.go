2e559d54cd9d1df1b56ad2bc274bd71d0ec6c374
Makarov Andrey <winandfx@gmail.com>
2022-08-17 23:09:25 +0300

    Server kill

--- src/linkedin/go-essential/kill_service/kill_service.go
+++ src/linkedin/go-essential/kill_service/kill_service.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func killServer(pidFile string) error {
+	// https://gobyexample.com/reading-files
+	content, err := os.ReadFile(pidFile)
+	if err != nil {
+		return err
+	}
+	str := strings.TrimSpace(string(content))
+	// Max PID is 2^22 https://stackoverflow.com/q/6294133/maximum-pid-in-linux
+	// Max uint32 is 4`294`967`295 https://stackoverflow.com/a/6878625/1119602
+	pid, err := strconv.ParseUint(str, 0, 32)
+	if err != nil {
+		return err
+	}
 	fmt.Printf("killing server with pid=%d\n", pid)
 	return nil
 }
 
 func main() {
-	if err := killServer("sever.pid"); err != nil {
+	if err := killServer("server.pid"); err != nil {
 		log.Fatalf("error: %s", err)
 	}
 }
