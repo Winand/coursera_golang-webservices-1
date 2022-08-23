b59781aa0335c2f08c5b763c20323ff031ee2538
Andrey Makarov <winandfx@gmail.com>
2022-08-23 11:50:14 +0300

    Testify sqrt: defer close, use struct

--- src/linkedin/go-essential/testing/sqrt_test.go
+++ src/linkedin/go-essential/testing/sqrt_test.go
@@ -2,6 +2,7 @@ package sqrt
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -18,18 +19,22 @@ type testCase struct {
 func TestMany(t *testing.T) {
 	f, err := os.Open("testcases.csv")
 	require.NoError(t, err)
+	defer f.Close()
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	require.NoError(t, err)
+
 	for _, v := range records {
 		fIn, err := strconv.ParseFloat(strings.TrimSpace(v[0]), 64)
 		require.NoError(t, err)
 		fExp, err := strconv.ParseFloat(strings.TrimSpace(v[1]), 64)
 		require.NoError(t, err)
-		t.Run(v[0], func(t *testing.T) {
-			out, err := Sqrt(fIn)
+		tst := testCase{value: fIn, expected: fExp}
+
+		t.Run(fmt.Sprintf("%f", tst.value), func(t *testing.T) {
+			out, err := Sqrt(tst.value)
 			require.NoError(t, err)
-			require.InDelta(t, fExp, out, 0.001)
+			require.InDelta(t, tst.expected, out, 0.001)
 		})
 	}
 }
